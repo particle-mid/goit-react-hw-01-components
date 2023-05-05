a2d8aaa7ca344ae7e724d43e49e8069c552c7ef2
particle-mid <particle1112@gmail.com>
2023-05-06 00:38:40 +0300

    constants

---
+++ src/components/constants/Global.Styles.jsx
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+}
+body,
+html {
+  height: 100%;
+}
+body {
+  background-color: pink;
+}
+img,
+picture,
+svg {
+  display: block;
+  max-width: 100%;
+  height: auto;
+}
+input,
+button,
+textarea,
+select {
+  font: inherit;
+}
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+}
+ul,
+ol {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+}
+button {
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}`;
---
+++ src/components/constants/theme.js
@@ -0,0 +1,24 @@
+export const theme = Object.freeze({
+    colors: {
+      white: '#ffffff',
+      tableBg: '#d7d7d7',
+      grey: '#4d5d53',
+      pinkBg: '#faafe4b2',
+      darkPink: '#f974d1ce',
+      pink: 'pink',
+      green: '#4caf50',
+      red: '#f44336',
+      tableHead: '#fbb4e6',
+      primaryText: '#2b2b2b',
+      secondaryText: '#757575',
+      tableText: '#afb1b8',
+      boxShadow:
+        '0px 1px 3px rgba(0, 0, 0, 0.22), 0px 2px 2px rgba(0, 0, 0, 0.24), 0px 2px 1px rgba(0, 0, 0, 0.32)',
+      profileShadow: '0 0 10px rgba(0,0,0,.3)',
+    },
+    fonts: {
+      secondary: "'Work Sans', sans-serif",
+    },
+    spacing: value => `${4 * value}px`,
+  });
+  
